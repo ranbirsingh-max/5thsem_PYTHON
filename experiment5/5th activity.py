cd83075b72cb3403f5a7bf7747075c5dd271dd58
Ranbir Singh <106223825+ranbirsingh-max@users.noreply.github.com>
2023-11-13 23:13:47 +0530

    Update and rename 5th activity to 5th activity.py

--- experiment5/5th activity.py
+++ experiment5/5th activity.py
@@ -1,6 +1,6 @@
-Experiment5: Write a program to reverse every Kth row in
+'''Experiment5: Write a program to reverse every Kth row in
 a matrix.
-CODE:
+CODE:'''
 test_list=[[5,3,2],[8,6,3],[3,5,2],[3,6],[3,7,4],[2,9]]
 print("The original list is:" + str(test_list))
 k=int(input("Enter the row value want to reverse:"))
